9e553b4d68e28c902a9925f007e252121ed3cf9c
accmanage52 <accmanage52@gmail.com>
2025-08-27 01:10:05 +0530

    Update RecordTable.jsx

--- components/RecordTable.jsx
+++ components/RecordTable.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
+import { supabase } from '../lib/supabaseClient'
 
 export default function RecordTable() {
   const [records, setRecords] = useState([])

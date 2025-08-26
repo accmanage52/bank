0ee9b0e172d70a0570be04b9547db086b38ad8cc
accmanage52 <accmanage52@gmail.com>
2025-08-27 00:58:59 +0530

    Create Navbar.js

---
+++ components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="bg-gradient-to-r from-purple-600 to-indigo-600 p-4 text-white shadow-lg">
+      <div className="max-w-7xl mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-xl">Bank Data App</h1>
+      </div>
+    </nav>
+  );
+}
